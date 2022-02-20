014e2a3d04c8cf4881a3dc4bee1c56f5aee6ad86
julienbenincasa <benincasajuju@gmail.com>
2022-02-21 00:43:21 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- router view and links-->
+  <div>Hello World</div>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<style></style>
